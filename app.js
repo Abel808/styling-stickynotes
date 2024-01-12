6a9464665a20c53d5c4c3d77822d70bf0a3f51e4
Abel808 <able.kodes@gmail.com>
2024-01-12 21:50:54 +0530

    added sticky

---
+++ app.js
@@ -0,0 +1,16 @@
+
+
+  
+    let targetDiv = document.getElementById("DivId");
+ 
+    if (targetDiv) {
+      
+      targetDiv.style.backgroundColor = "yellow";
+      targetDiv.style.margin = "20px";
+      targetDiv.style.padding = "10px";
+      targetDiv.style.fontSize = "18px";
+      targetDiv.style.fontWeight = "bold";
+      targetDiv.style.height = "200px";
+      targetDiv.style.width = "300px";
+      targetDiv.innerText='Hello, Im a div!'
+    }
